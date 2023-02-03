d663abd100ac6883926b04667754d58df24091d9
-ASUS- <Isaevaa@tut.by>
2023-02-03 22:11:26 +0300

    Isaeva_2023

--- Isaeva/project/models.py
+++ Isaeva/project/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project (models.Model):
+    title = models.CharField(max_length=100)  # первое поле типа чар-символ с ограничением в 100символов
+    description = models.TextField()    # поле с описанием без ограничением
+    technology = models.CharField(max_length=50)  # поле с использоваными технологиями с 50символами
+    image = models.FileField(upload_to='img/')  # картинка
